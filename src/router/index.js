f90015ccd3b0d9485c6c3af78ef58207a19dd7ac
Scrlkk <shevaazki6@gmail.com>
2025-05-14 15:54:45 +0700

    template

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/login/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomeView,
+      component: Login,
     },
   ],
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return {
-        ...savedPosition,
-        behavior: 'smooth',
-      }
-    } else {
-      return { top: 0, behavior: 'smooth' }
-    }
-  },
 })
 
 export default router
---
+++ src/views/login/Login.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import GuestLayout from '@/layouts/GuestLayout.vue'
+</script>
+
+<template>
+  <GuestLayout />
+  <div class="z-2 text-8xl">
+    sadasdasdas
+  </div>
+</template>
